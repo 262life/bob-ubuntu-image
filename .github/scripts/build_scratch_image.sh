28dabb494f1b50bd9759bef32e9f2910e69ca504
Robert Liotta <44572134+rsliotta@users.noreply.github.com>
2021-11-11 19:44:53 -0500

    fix

--- .github/scripts/build_scratch_image.sh
+++ .github/scripts/build_scratch_image.sh
@@ -7,6 +7,9 @@ mkdir -p distro/pre-work
 chmod 755 .github/scripts/init.sh 
 LATEST=false TAG=interim PUBLISH=false DOCKERFILE=dockerfiles/Dockerfile-core-prework .github/scripts/init.sh bob-pre-work
 
+which python
+ls /usr/bin/pyth*
+
 docker save -o distro/pre-work/latest-focal.tar bob-pre-work:interim
 mkdir -p distro/pre-work/extract
 sudo .github/scripts/extract_image.py distro/pre-work/latest-focal.tar distro/pre-work/extract
