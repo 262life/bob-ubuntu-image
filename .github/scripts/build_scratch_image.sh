70d20c4b498816ba1ace45771fe7ec609d32fffe
Robert Liotta <bob@bobdot.me>
2021-12-16 10:02:01 -0500

    test

--- .github/scripts/build_scratch_image.sh
+++ .github/scripts/build_scratch_image.sh
@@ -14,7 +14,7 @@ ls /usr/bin/pyth*
 
 docker save -o distro/pre-work/latest-focal.tar bob-pre-work:interim
 mkdir -p distro/pre-work/extract
-sudo .github/scripts/extract_image.py distro/pre-work/latest-focal.tar distro/pre-work/extract
+sudo chmod 755 .github/scripts/extract_image.py; sudo .github/scripts/extract_image.py distro/pre-work/latest-focal.tar distro/pre-work/extract
 cd distro/pre-work/extract || exit
 sudo rm -rf work
 sudo tar zcf ../../ubuntu-focal-${ARCH}-latest.tar.gz ./*
